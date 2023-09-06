e1249a99ed822c12a388ca4199619cb0cf068aec
Kingskylark <whisper2ku@gmail.com>
2023-09-06 11:44:43 +0100

    Documenation

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
-
+// Import necessary modules from React and React Native
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import {
    Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Modal,
-  Button,
-  StatusBar,
-  FlatList,
-  StyleSheet,
-  Animated,
+   View,
+   TextInput,
+   TouchableOpacity,
+   Modal,
+   Button,
+   StatusBar,
+   FlatList,
+   Animated,
 } from 'react-native';
+
+// Import an icon component from the Expo vector icons library
 import { EvilIcons } from '@expo/vector-icons';
+
+// Import the SQLite library
 import * as SQLite from 'expo-sqlite';
+
+// Import a predefined list of computer terms
 import computerTerms from './ComputerTerms';
-import { styles } from './style'; 
+
+// Import custom styles
+import { styles } from './style';
+
+// Import BackHandler from React Native
 import { BackHandler } from 'react-native';
 
+// Define a functional component called WordItem to render individual dictionary words
 const WordItem = ({ item, onPress }) => {
   return (
     <TouchableOpacity onPress={() => onPress(item)} style={styles.wordItem}>
@@ -28,16 +38,19 @@ const WordItem = ({ item, onPress }) => {
   );
 };
 
+// Define the main functional component of the app
 const App = () => {
+  // State variables to manage various aspects of the app
   const [searchText, setSearchText] = useState('');
   const [selectedWord, setSelectedWord] = useState(null);
   const [bookmarksVisible, setBookmarksVisible] = useState(false);
   const [bookmarks, setBookmarks] = useState([]);
   const [dictionaryData, setDictionaryData] = useState([]);
 
+  // Function to create the SQLite database and insert computer terms into it
   const createAndInsertComputerTerms = () => {
     db.transaction((tx) => {
-      // Create the 'Computer Terms' table if it doesn't exist
+      // Create the 'dictionary' table if it doesn't exist
       tx.executeSql(
         `CREATE TABLE IF NOT EXISTS dictionary (
           word TEXT PRIMARY KEY,
@@ -59,6 +72,7 @@ const App = () => {
     });
   };
 
+  // Function to insert data into the 'dictionary' table
   const insertData = () => {
     // Now you can use the imported computerTerms for insertion
     db.transaction((tx) => {
@@ -111,26 +125,33 @@ const App = () => {
     });
   };
 
+  // Function to handle the hardware back button press
   const handleBackButtonPress = () => {
     closeModal();
     return true; // Prevent default behavior (exiting the app)
   };
 
+  // Open the SQLite database
   const db = SQLite.openDatabase('dictionary.db');
+
   useEffect(() => {
+    // Call functions for database setup and data insertion when the component mounts
     createAndInsertComputerTerms();
     insertData();
+
+    // Add a hardware back button listener
     const backHandler = BackHandler.addEventListener(
       'hardwareBackPress',
       handleBackButtonPress
     );
 
+    // Remove the back button listener when the component unmounts
     return () => {
-      // Remove the back button listener when the component unmounts
       backHandler.remove();
     };
   }, []);
 
+  // Function to filter dictionary data based on the search text
   const filterData = () => {
     if (searchText.trim() === '') {
       return [];
@@ -140,18 +161,22 @@ const App = () => {
     );
   };
 
+  // Function to render each item in the FlatList
   const renderItem = ({ item }) => (
     <WordItem item={item} onPress={showWordDetails} />
   );
 
+  // Function to display word details in a modal
   const showWordDetails = (word) => {
     setSelectedWord(word);
   };
 
+  // Function to close the modal
   const closeModal = () => {
     setSelectedWord(null);
   };
 
+  // Function to toggle bookmarks for a word
   const toggleBookmark = (word) => {
     if (bookmarks.includes(word)) {
       const updatedBookmarks = bookmarks.filter((item) => item !== word);
@@ -161,8 +186,10 @@ const App = () => {
     }
   };
 
+  // Function to check if a word is bookmarked
   const isWordBookmarked = (word) => bookmarks.includes(word);
 
+  // Function to render a bookmarked word
   const renderBookmarkedWord = ({ item }) => {
     const foundWord = dictionaryData.find((word) => word.word === item);
 
@@ -177,6 +204,7 @@ const App = () => {
     return null; // Handle the case when the word is not found
   };
 
+  // JSX code for rendering the main UI of the app
   return (
     <View style={styles.container}>
       <StatusBar animated={true} backgroundColor="#0047ab" barStyle="light" />
@@ -198,56 +226,56 @@ const App = () => {
         />
       )}
 
-    <Modal visible={selectedWord !== null} animationType="slide">
-  <View style={styles.modalContainer}>
-    <View style={styles.modalHeader}>
-      <Text style={styles.modalTitle}>{selectedWord?.word}</Text>
-      <Text style={styles.lexicalCategory}>
-        {selectedWord?.lexicalCategory}
-      </Text>
-    </View>
-    <View style={styles.modalBody}>
-      <Text style={styles.definition}>
-        Definition: {selectedWord?.definition}
-      </Text>
-      <Text style={styles.pronunciation}>
-        Pronunciation: {selectedWord?.pronunciation}
-      </Text>
-      <Text style={styles.synonym}>
-        Synonym: {selectedWord?.synonym || 'N/A'}
-      </Text>
-      <Text style={styles.antonym}>
-        Antonym: {selectedWord?.antonym || 'N/A'}
-      </Text>
-      <Text style={styles.sentence}>Sentence: {selectedWord?.sentence}</Text>
-    </View>
-    <View style={styles.modalFooter}>
-      <TouchableOpacity
-        style={[
-          styles.bookmarkButton,
-          {
-            backgroundColor: isWordBookmarked(selectedWord?.word)
-              ? 'red'
-              : 'blue',
-          },
-        ]}
-        onPress={() => toggleBookmark(selectedWord?.word)}
-      >
-        <Text style={styles.bookmarkButtonText}>
-          {isWordBookmarked(selectedWord?.word)
-            ? 'Remove Bookmark'
-            : 'Bookmark'}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.goBackButton}
-        onPress={closeModal}
-      >
-        <Text style={styles.goBackButtonText}>Go Back</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-</Modal>
+      <Modal visible={selectedWord !== null} animationType="slide">
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalTitle}>{selectedWord?.word}</Text>
+            <Text style={styles.lexicalCategory}>
+              {selectedWord?.lexicalCategory}
+            </Text>
+          </View>
+          <View style={styles.modalBody}>
+            <Text style={styles.definition}>
+              Definition: {selectedWord?.definition}
+            </Text>
+            <Text style={styles.pronunciation}>
+              Pronunciation: {selectedWord?.pronunciation}
+            </Text>
+            <Text style={styles.synonym}>
+              Synonym: {selectedWord?.synonym || 'N/A'}
+            </Text>
+            <Text style={styles.antonym}>
+              Antonym: {selectedWord?.antonym || 'N/A'}
+            </Text>
+            <Text style={styles.sentence}>Sentence: {selectedWord?.sentence}</Text>
+          </View>
+          <View style={styles.modalFooter}>
+            <TouchableOpacity
+              style={[
+                styles.bookmarkButton,
+                {
+                  backgroundColor: isWordBookmarked(selectedWord?.word)
+                    ? 'red'
+                    : 'blue',
+                },
+              ]}
+              onPress={() => toggleBookmark(selectedWord?.word)}
+            >
+              <Text style={styles.bookmarkButtonText}>
+                {isWordBookmarked(selectedWord?.word)
+                  ? 'Remove Bookmark'
+                  : 'Bookmark'}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.goBackButton}
+              onPress={closeModal}
+            >
+              <Text style={styles.goBackButtonText}>Go Back</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
 
       <TouchableOpacity
         style={styles.bookmarkIconContainer}
@@ -257,15 +285,14 @@ const App = () => {
 
       {bookmarksVisible && (
         <View style={styles.bookmarkedWordsContainer}>
-  <Text style={styles.bookmarksTitle}>Bookmarked Words:</Text>
-  <FlatList
-    data={bookmarks}
-    renderItem={renderBookmarkedWord}
-    keyExtractor={(item, index) => index.toString()}
-    style={styles.bookmarkedWordItem}
-  />
-</View>
-
+          <Text style={styles.bookmarksTitle}>Bookmarked Words:</Text>
+          <FlatList
+            data={bookmarks}
+            renderItem={renderBookmarkedWord}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.bookmarkedWordItem}
+          />
+        </View>
       )}
     </View>
   );
--- style.js
+++ style.js
@@ -1,25 +1,33 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
+  // Styling for the main container of the application
   container: {
     flex: 1,
     backgroundColor: '#f8f8f8',
-    
   },
+  
+  // Styling for the search container
   searchContainer: {
     backgroundColor: '#0073cf',
     padding: 10,
-    marginTop:20,
+    marginTop: 20,
   },
+  
+  // Styling for the search input field
   searchInput: {
     fontSize: 18,
     padding: 10,
     backgroundColor: '#fff',
     borderRadius: 5,
   },
+  
+  // Styling for a list container
   list: {
     padding: 16,
   },
+  
+  // Styling for an item in the word list
   wordItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -28,21 +36,28 @@ export const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderRadius: 5,
   },
+  
+  // Styling for the word text
   word: {
     fontSize: 18,
     color: '#333',
   },
+  
+  // Styling for a divider line
   divider: {
     height: 1,
     backgroundColor: '#ddd',
     marginVertical: 10,
   },
-   bookmarkIconContainer: {
-    position: 'absolute', 
-    right: 10, 
-    bottom: 10, 
+  
+  // Styling for a bookmark icon container
+  bookmarkIconContainer: {
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
   },
-
+  
+  // Styling for a button
   button: {
     display: 'flex',
     height: 60,
@@ -53,61 +68,84 @@ export const styles = StyleSheet.create({
     backgroundColor: '#2AC062',
     shadowColor: '#2AC062',
     shadowOpacity: 0.5,
-    shadowOffset: { 
-      height: 10, 
-      width: 0 
-    },
+    shadowOffset: { height: 10, width: 0 },
     shadowRadius: 25,
   },
+  
+  // Styling for button text
   buttonText: {
     color: '#FFFFFF',
     fontSize: 22,
   },
+  
+  // Styling for the modal container
   modalContainer: {
     flex: 1,
     backgroundColor: 'white',
     padding: 16,
   },
+  
+  // Styling for the modal header
   modalHeader: {
     alignItems: 'center',
   },
+  
+  // Styling for the modal title
   modalTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 8,
   },
+  
+  // Styling for lexical category text
   lexicalCategory: {
     fontSize: 18,
     color: 'gray',
   },
+  
+  // Styling for the modal body
   modalBody: {
     marginVertical: 16,
   },
+  
+  // Styling for word definition text
   definition: {
     fontSize: 16,
     marginBottom: 8,
   },
+  
+  // Styling for word pronunciation text
   pronunciation: {
     fontSize: 16,
     marginBottom: 8,
   },
+  
+  // Styling for synonym text
   synonym: {
     fontSize: 16,
     marginBottom: 8,
   },
+  
+  // Styling for antonym text
   antonym: {
     fontSize: 16,
     marginBottom: 8,
   },
+  
+  // Styling for sentence text
   sentence: {
     fontSize: 16,
     marginBottom: 8,
   },
+  
+  // Styling for the modal footer
   modalFooter: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  
+  // Styling for the bookmark button
   bookmarkButton: {
     flex: 1,
     backgroundColor: 'blue',
@@ -115,10 +153,14 @@ export const styles = StyleSheet.create({
     borderRadius: 8,
     alignItems: 'center',
   },
+  
+  // Styling for the bookmark button text
   bookmarkButtonText: {
     color: 'white',
     fontWeight: 'bold',
   },
+  
+  // Styling for the go back button
   goBackButton: {
     flex: 1,
     backgroundColor: 'red',
@@ -127,27 +169,33 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     marginLeft: 16,
   },
+  
+  // Styling for the go back button text
   goBackButtonText: {
     color: 'white',
     fontWeight: 'bold',
   },
+  
+  // Styling for the container of bookmarked words
   bookmarkedWordsContainer: {
     backgroundColor: '#F5F5F5',
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
   },
+  
+  // Styling for the title of bookmarked words
   bookmarksTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
   },
+  
+  // Styling for an item in the bookmarked words list
   bookmarkedWordItem: {
     backgroundColor: 'white',
     padding: 10,
     marginBottom: 5,
     borderRadius: 5,
   },
-
-
 });
